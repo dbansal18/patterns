ec64e8b4f2bbc6505e48a03e5d2c2a0d67b4680f
dbansal18 <bansaldeepanshu08@gmail.com>
2019-02-22 15:36:37 +0530

    2

--- 6.c
+++ 6.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+void main()
+{
+    int i,j,k;
+
+    for(i=1;i<5;i++)
+    {
+        k=1;
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",k);
+            k++;
+        }
+        for(j=i;j<0;j--)
+        {
+            printf("%d",k);
+            k--;
+        }
+        printf("\n");
+    }
+}
